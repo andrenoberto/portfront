b9d76ab50da3170c93e91a14fedaa8f91260bd1d
Andre Noberto <joao.andre.souza@accenture.com>
2018-04-02 21:51:00 -0300

    Added task to minify images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const imageMin = require('gulp-imagemin');
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'imageMin', 'watch']);
 
 gulp.task('sass', () => {
     gulp.src('./assets/sass/**/*.scss')
@@ -9,6 +10,15 @@ gulp.task('sass', () => {
         .pipe(gulp.dest('./src/assets/css/'));
 });
 
+gulp.task('imageMin', () => {
+    gulp.src('./assets/images/*')
+        .pipe(imageMin([
+            imageMin.optipng({optimizationLevel: 7})
+        ]))
+        .pipe(gulp.dest('./src/assets/images/'));
+});
+
 gulp.task('watch', () => {
     gulp.watch('./assets/sass/**/*.scss', ['sass']);
+    gulp.watch('./assets/images/*', ['imageMin']);
 });
