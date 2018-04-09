36860da8435bbed24816536c4edefe7dece1b7f6
Andre Noberto <joao.andre.souza@accenture.com>
2018-04-08 23:20:48 -0300

    BrowserSync added to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
 const imageMin = require('gulp-imagemin');
+const sass = require('gulp-sass');
 
 gulp.task('default', ['font-awesome', 'imageMin', 'sass', 'watch']);
 
 gulp.task('font-awesome', () => {
     gulp.src('./node_modules/@fortawesome/fontawesome-free-webfonts/scss/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./src/assets/font-awesome/css/'));
+        .pipe(sass({outputStyle: 'compressed'})).on('error', sass.logError)
+        .pipe(gulp.dest('./src/assets/font-awesome/css/'))
 });
 
 gulp.task('imageMin', () => {
@@ -15,16 +16,29 @@ gulp.task('imageMin', () => {
         .pipe(imageMin([
             imageMin.optipng({optimizationLevel: 7})
         ]))
-        .pipe(gulp.dest('./src/assets/images/'));
+        .pipe(gulp.dest('./src/assets/images/'))
 });
 
 gulp.task('sass', () => {
     gulp.src('./assets/sass/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./src/assets/css/'));
+        .pipe(sass({outputStyle: 'compressed'})).on('error', sass.logError)
+        .pipe(gulp.dest('./src/assets/css/'))
+});
+
+gulp.task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+
+    gulp.watch('./src/**/*').on('change', browserSync.reload);
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
+    gulp.watch('./assets/images/*', ['imageMin']);
 });
 
 gulp.task('watch', () => {
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
     gulp.watch('./assets/sass/**/*.scss', ['sass']);
     gulp.watch('./assets/images/*', ['imageMin']);
 });
