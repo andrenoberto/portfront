16e9779bae3c68ad3edd192f247e2f66e22e44b0
Andre Noberto <joao.andre.souza@accenture.com>
2018-05-08 01:10:07 -0300

    Adding accessibility

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,18 +3,26 @@
 
     const btn = document.querySelector('.header-nav__navbar');
     const html = document.querySelector('html');
+    const menu = document.getElementById('main-menu');
     const openedMenuClass = 'menu-opened';
     let menuOpened = false;
 
     btn.addEventListener('click', toggleMenu);
     html.addEventListener('click', (e) => {
         if (e.target === html && menuOpened) {
-            closeMenu();
+            toggleMenu();
+        }
+    });
+    html.addEventListener('keyup', (e) => {
+        if (e.key === 'Escape' && menuOpened) {
+            toggleMenu();
         }
     });
 
-    function toggleMenu(e) {
+    function toggleMenu() {
         menuOpened ? closeMenu() : openMenu();
+        btn.setAttribute('aria-expanded', menuOpened.toString());
+        menu.setAttribute('aria-expanded', menuOpened.toString());
     }
 
     function closeMenu() {
