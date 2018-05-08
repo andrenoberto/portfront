e68bc865eedc912e4ad3bf5983e3a54c7f506974
Andre Noberto <joao.andre.souza@accenture.com>
2018-05-08 00:51:28 -0300

    Improved close menu action and added event listener to close menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -7,18 +7,24 @@
     let menuOpened = false;
 
     btn.addEventListener('click', toggleMenu);
+    html.addEventListener('click', (e) => {
+        if (e.target === html && menuOpened) {
+            closeMenu();
+        }
+    });
 
     function toggleMenu(e) {
         menuOpened ? closeMenu() : openMenu();
     }
 
     function closeMenu() {
-        menuOpened = false;
+        btn.blur();
         html.classList.remove(openedMenuClass);
+        menuOpened = false;
     }
 
     function openMenu() {
-        menuOpened = true;
         html.classList.add(openedMenuClass);
+        menuOpened = true;
     }
 })();
